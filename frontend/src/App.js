4981b2b46292aa68052d9c2a2de83fc783a01707
Liad Hossain <liadhossain@gmail.com>
2025-05-25 09:45:23 +0600

    add: added successfull and error notifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,24 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Chat from './pages/Chat/Chat';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 
 function App() {
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/chat" element={<Chat />}/>
