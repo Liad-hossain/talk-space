779c92ae952f06654773d8df25811b010a1f927e
Liad Hossain <liadhossain@gmail.com>
2025-06-26 10:14:29 +0600

    update: updated entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,15 +2,15 @@
 set -e
 
 # Run DB migrations
-python ./src/manage.py migrate --noinput
+python ./src/manage.py migrate --noinput --verbosity 3
 
-python ./src/manage.py collectstatic --noinput
+python ./src/manage.py collectstatic --noinput --clear
 
 if [[ "$ENVIRONMENT" == "DEBUG" || "$ENVIRONMENT" == "DEVELOPMENT" ]];
 then
     python ./src/manage.py runserver 0.0.0.0:8000
 
-elif [["$ENVIRONMENT" == "PRODUCTION" ]];
+elif [["$ENVIRONMENT" == "LIVE" ]];
 then
     (gunicorn core.asgi:application -w 3 -k uvicorn.workers.UvicornWorker --chdir src --bind 0.0.0.0:8000 --access-logfile - --error-logfile -)
 
@@ -21,3 +21,5 @@ fi
 
 # Run the Django server
 exec "$@"
+
+python ./src/manage.py showmigrations
