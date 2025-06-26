b6e86edaea941f5c2f23b1a3514eeeb980799c6a
Liad Hossain <liadhossain@gmail.com>
2025-06-26 10:25:07 +0600

    update: added makemigrations command inside entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Run DB migrations
+python ./src/manage.py makemigrations --noinput
 python ./src/manage.py migrate --noinput --verbosity 3
 
 python ./src/manage.py collectstatic --noinput --clear
@@ -10,7 +11,7 @@ if [[ "$ENVIRONMENT" == "DEBUG" || "$ENVIRONMENT" == "DEVELOPMENT" ]];
 then
     python ./src/manage.py runserver 0.0.0.0:8000
 
-elif [["$ENVIRONMENT" == "LIVE" ]];
+elif [[ "$ENVIRONMENT" == "LIVE" ]];
 then
     (gunicorn core.asgi:application -w 3 -k uvicorn.workers.UvicornWorker --chdir src --bind 0.0.0.0:8000 --access-logfile - --error-logfile -)
 
