cf72777f5d25fea34c42c0d8ef023a889e0e6f1a
Liad Hossain <liadhossain@gmail.com>
2025-06-27 20:17:18 +0600

    add: added a tiny webserver to export a port in background worker on render

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,9 +8,11 @@ if [[ "$SERVICE" == "app" ]]; then
 
 elif [[ "$SERVICE" == "redis-listener" ]]; then
   python ./src/manage.py start_redis_inbox_listener
+  exec python -m http.server "$PORT"
 
 elif [[ "$SERVICE" == "celery-heartbeat" ]]; then
   PYTHONPATH=/app/src celery -A core worker --loglevel=INFO -Q heartbeat --hostname=basic@%h
+  exec python -m http.server "$PORT"
 
 else
   echo "❌ Unknown SERVICE: $SERVICE"
