2a31b9dd9c11e68f8266702522927f4dbde6c770
Liad Hossain <liadhossain@gmail.com>
2025-06-27 09:18:12 +0600

    add: added different entrypoint for app and another services

---
+++ backend/app_entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# Run DB migrations
+python ./src/manage.py migrate --noinput --verbosity 3
+
+python ./src/manage.py collectstatic --noinput --clear
+
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python ./src/manage.py createsuperuser --no-input
+fi
+
+if [[ "$ENVIRONMENT" == "DEBUG" || "$ENVIRONMENT" == "DEVELOPMENT" ]];
+then
+    python ./src/manage.py runserver 0.0.0.0:8000
+
+elif [[ "$ENVIRONMENT" == "LIVE" ]];
+then
+    (gunicorn core.asgi:application -w 3 -k uvicorn.workers.UvicornWorker --chdir src --bind 0.0.0.0:8000 --access-logfile - --error-logfile -)
+
+else
+    (gunicorn core.asgi:application -w 3 -k uvicorn.workers.UvicornWorker --chdir src --bind 0.0.0.0:8000 --access-logfile - --error-logfile -)
+
+fi
+
+# Run the Django server
+exec "$@"
+
+python ./src/manage.py showmigrations
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,31 +1,12 @@
-#!/bin/bash
-set -e
-
-# Run DB migrations
-python ./src/manage.py migrate --noinput --verbosity 3
-
-python ./src/manage.py collectstatic --noinput --clear
-
-
-if [[ $CREATE_SUPERUSER ]];
+if [[ $SERVICE == "app" ]];
 then
-  python ./src/manage.py createsuperuser --no-input
+  ./entrypoint.sh
+
+elif [[ $SERVICE == "redis-listener" ]];
+then
+  python ./src/manage.py start_redis_inbox_listener
+
+elif [[ $SERVICE == "celery-heartbeat" ]];
+then
+  PYTHONPATH=/app/src celery -A core worker --loglevel=INFO -Q heartbeat --hostname=basic@%h
 fi
-
-if [[ "$ENVIRONMENT" == "DEBUG" || "$ENVIRONMENT" == "DEVELOPMENT" ]];
-then
-    python ./src/manage.py runserver 0.0.0.0:8000
-
-elif [[ "$ENVIRONMENT" == "LIVE" ]];
-then
-    (gunicorn core.asgi:application -w 3 -k uvicorn.workers.UvicornWorker --chdir src --bind 0.0.0.0:8000 --access-logfile - --error-logfile -)
-
-else
-    (gunicorn core.asgi:application -w 3 -k uvicorn.workers.UvicornWorker --chdir src --bind 0.0.0.0:8000 --access-logfile - --error-logfile -)
-
-fi
-
-# Run the Django server
-exec "$@"
-
-python ./src/manage.py showmigrations
